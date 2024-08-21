829c39936fbaa6e5e29dfc0bc090210b69d82498
gmsplsxy <gmsplsxy@icloud.com>
2024-08-22 00:21:23 +0900

    paging

--- include/mm.h
+++ include/mm.h
@@ -1,18 +1,24 @@
 #ifndef __MM_H__
 #define __MM_H__
 
-#define PAGE_SHIFT      12
+#include "arch/rpi/base.h"
+
+#define PAGE_SHIFT        12
 #define TABLE_SHIFT       9
 #define SECTION_SHIFT     (PAGE_SHIFT + TABLE_SHIFT)
 
 #define PAGE_SIZE         (1 << PAGE_SHIFT)
 #define SECTION_SIZE      (1 << SECTION_SHIFT)
 
-#define LOW_MEMORY                (2 * SECTION_SIZE)
+#define LOW_MEMORY        (2 * SECTION_SIZE)
+#define HIGH_MEMORY       PBASE
 
-#ifndef __ASSEMBLER__
+#define PAGING_MEMORY     (HIGH_MEMORY - LOW_MEMORY)
+#define PAGING_PAGES      (PAGING_MEMORY / PAGE_SIZE)
 
 void memzero(unsigned long src, unsigned long n);
-#endif
+unsigned long get_free_page();
+void free_page(unsigned long p);
+
 
 #endif //__MM_H__
